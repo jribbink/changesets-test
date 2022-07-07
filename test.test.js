e9944f19585277626638d3730123c82d9b8b59f7
Jordan Ribbink <jribbink@telus.net>
2022-07-07 14:36:40 -0700

    add test

---
+++ test.test.js
@@ -0,0 +1,27 @@
+import path from "path";
+import { init, emulator, executeScript } from "flow-js-testing";
+
+test("test", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start();
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
