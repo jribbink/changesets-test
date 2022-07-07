a7ad645afd6c0fdeae5ba43d2d0a82bd916d9215
Jordan Ribbink <jribbink@telus.net>
2022-07-07 15:57:55 -0700

    fail test

--- test.test.js
+++ test.test.js
@@ -54,3 +54,263 @@ test("test", async () => {
   // Stop emulator instance
   await emulator.stop();
 });
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
