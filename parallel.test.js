878657a996a902c20242bc8dd5367f4d4942572d
Jordan Ribbink <jribbink@telus.net>
2022-07-07 15:47:20 -0700

    parallel test

---
+++ parallel.test.js
@@ -0,0 +1,56 @@
+import path from "path";
+import { init, emulator, executeScript } from "@onflow/flow-js-testing";
+
+test("testp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start();
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
+
+test("testpp", async () => {
+  const basePath = path.resolve(__dirname, "../cadence");
+
+  // Init framework
+  init(basePath);
+  // Start emulator
+  await emulator.start({ restPort: 8080, adminPort: 8888, grpcPort: 3569 });
+
+  console.log(emulator.adminPort);
+
+  // Define code and arguments we want to pass
+  const code = `
+    pub fun main(message: String): Int{
+      log(message)
+
+      return 42
+    }
+  `;
+  const args = ["Hello, from Cadence"];
+
+  const [result, e] = await executeScript({ code, args });
+  expect(result).toBe(42);
+
+  // Stop emulator instance
+  await emulator.stop();
+});
